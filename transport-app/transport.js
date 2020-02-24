bc2ddbfeaef71eefeef2b654f422a2a67dca0096
Zerneax <d.chris59@gmail.com>
2020-02-24 22:36:26 +0100

    un peu de clean code

--- transport-app/transport.js
+++ transport-app/transport.js
@@ -3,7 +3,7 @@ var eb = vertx.eventBus();
 
 var orderReadyToGo = []
 
-eb.consumer("ready.to.go", function (message) {
+eb.consumer("order.ready.to.go", function (message) {
   console.log("\n\n New order ready to go: " + message.body());
   orderReadyToGo.push(message.body());
 });
@@ -12,7 +12,7 @@ eb.consumer("ready.to.go", function (message) {
 vertx.setPeriodic(10000, function (id) {
   // This handler will get called every second
   if (orderReadyToGo.length != 0) {
-    eb.send('delivered', orderReadyToGo[0]);
-
+    console.log("\n\n Order " + orderReadyToGo[0] + " has been deliver.")
+    eb.send('order.delivered', orderReadyToGo.shift());
   }
 });
